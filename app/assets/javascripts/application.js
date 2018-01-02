6cffd46467d4b5de42fb3fc152945d32a7dd9d51
rahul2240 <rahulsingh2240@gmail.com>
2018-01-02 16:59:56 +0530

    Working github charts

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,30 +11,7 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require highcharts
+//= require chartkick
 //= require turbolinks
 //= require_tree .
-
-function load(){
-var username = document.getElementById("username").value;
-var url = "https://api.github.com/users/" + username;
-var request = new XMLHttpRequest();
-
-request.onreadystatechange = function(){
-if(request.readyState==4)
-{
-var data = JSON.parse(request.responseText);
-document.getElementById("dis").style.display="block";
-document.getElementById("name").innerHTML = data.name;
-document.getElementById("user").innerHTML = data.login;
-document.getElementById("name2").innerHTML = data.name;
-document.getElementById("location").innerHTML = data.location;
-document.getElementById("loc").innerHTML = data.location;
-document.getElementById("bio").innerHTML = data.bio;
-document.getElementById("image").src = data.avatar_url;
-document.getElementById("image").style.width="50%";
-document.getElementById("image").style.height="50%";
-}
-}
-request.open('GET', url, true);
-request.send();
-}
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,36 @@
 class StaticPagesController < ApplicationController
+
+include HTTParty
+require 'rubygems'
+require 'nokogiri'
+require 'rest-client'
+
   def home
-  end
+    @username = params[:username]
+        if @username
+            @data = HTTParty.get('https://api.github.com/users/'+ @username)
+            @language = Hash.new(0)
+            a = 1
+            stop = 0
+            while(stop==0)
+            @url = HTTParty.get('https://api.github.com/users/'+ @username +'/repos?page='+ a.to_s)
+            if @url.count<30
+              stop=1
+            end
+
+            @url.each do |repo|
+              if repo['language']!=nil
+                x = repo['language'].downcase
+                @language[x] += 1
+              end
+             end
+
+             a+=1
+           end
+         end
+         
+
+       end
+
+
 end
