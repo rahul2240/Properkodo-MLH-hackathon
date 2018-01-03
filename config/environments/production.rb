464e4f6d2de41b7abad3c0fe80990b69b2d52676
rahul2240 <rahulsingh2240@gmail.com>
2018-01-03 16:18:05 +0530

    Backend ready and working

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,11 +25,13 @@ Rails.application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
+  config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
+  config.cache_classes = true
+  config.serve_static_assets = true
   config.assets.compile = true
-
+  config.assets.digest = true
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
