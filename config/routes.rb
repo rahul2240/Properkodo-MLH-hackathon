5a8851a762710e6119d37e4440791989c33184ae
rahul2240 <rahulsingh2240@gmail.com>
2018-01-03 14:59:01 +0530

    Setting up links

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-  root 'static_pages#home'
+  devise_scope :user do
+    root 'devise/registrations#new'
+  end
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
